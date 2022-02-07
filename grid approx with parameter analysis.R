cc8a8efac8270f8f69d482fe23f54b1e54d6bb7f
Thomas Taylor <t.h.taylor@outlook.com>
2022-02-07 22:26:49 +0000

    Add files via upload

--- grid approx with parameter analysis.R
+++ grid approx with parameter analysis.R
@@ -24,3 +24,29 @@ sum(samples > 0.5 & samples <0.75)/1e4 ####interval between two boundaries
 quantile(samples,0.8) #####compatibility interval, range of parameter values compatible with the model and data
 
 quantile(samples, c(.1,.9))###midel interval
+
+#####point interval not very useful, DISCARDS INFORMATION
+
+
+p_grid<-seq(from=0,to=1,length.out=1000)
+
+prior<-rep(1,1000)
+
+likelihood<-dbinom(3,size=3,prob=p_grid)
+
+posterior<-likelihood*prior
+posterior<-posterior/sum(posterior)
+samples<-sample(p_grid,size=1e4,replace=TRUE,prob=posterior)
+
+
+
+
+p_grid[which.max(posterior)]
+
+chainmode(samples,adj=.01)
+
+sum(posterior*abs(.5-p_grid)) ###weighted loss
+
+loss<-sapply(p_grid,function(d)sum(posterior*abs(d-p_grid)))
+
+p_grid[which.min(loss)]
