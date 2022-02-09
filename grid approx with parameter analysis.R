68453c51dccd85358a37775f3316d868e3439620
Thomas Taylor <t.h.taylor@outlook.com>
2022-02-09 22:50:57 +0000

    Add files via upload

--- grid approx with parameter analysis.R
+++ grid approx with parameter analysis.R
@@ -50,3 +50,19 @@ sum(posterior*abs(.5-p_grid)) ###weighted loss
 loss<-sapply(p_grid,function(d)sum(posterior*abs(d-p_grid)))
 
 p_grid[which.min(loss)]
+
+dbinom(0:2,size=2,prob=.7)
+
+
+rbinom(1 ,size=2,prob=.7) ###sample from a binom distribution
+
+dummy_w<-rbinom(1e5,size=2,prob=.7) #sample 10000 times
+table(dummy_w)/1e5 #proportional values retrieved
+
+
+dummy_w<-rbinom(1e5,size=9,prob=.1) #experiment with number of tosses
+freqtable<-table(dummy_w)/1e5 
+
+plot(freqtable)
+
+w<-rbinom(1e4,size=9,prob=samples) ##########this is called the posterior predictive distribution, once the size has been vectorised
